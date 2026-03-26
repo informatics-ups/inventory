c15cb0521ea560e8d0f4635f29a042ca9ef02060
sofaemha <mail@sofa.my.id>
2026-03-26 18:21:28 +0700

    feat: Implement internationalization with next-intl, including message loading, initial English translations, and page integration.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['100.104.25.60']
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
